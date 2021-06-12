181c2ae6030ce52d81aa7830a04f095ae74e6859
k.matsui <matsui.new@gmail.com>
2021-06-12 04:00:09 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -12,7 +13,6 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
         
         if @task.save
             flash[:success] = 'Task が正常に作成されました'
@@ -24,11 +24,9 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
-        @task = Task.find(params[:id])
         
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
@@ -40,12 +38,17 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'Task は正常に削除されました'
         redirect_to tasks_url
     end
+end
+
+private
+
+def set_task
+    @task = Task.find(params[:id])
 end
 
 # Strong Parameter
