e8eba065369260882dfe14a863228c8f1272c982
k.matsui <matsui.new@gmail.com>
2021-06-16 23:26:18 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to: 'tasks#index'
     
     resources :tasks
+    
+    get 'toppages/index'
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :create]
 end
