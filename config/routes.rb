b9bb2066237a6c7a3d1b1b838c6b0a4a58cf19bd
k.matsui <matsui.new@gmail.com>
2021-06-18 10:46:26 +0000

    saiteshutu_heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+    get 'users/index'
+    get 'users/show'
+    get 'users/new'
+    get 'users/create'
+      
     root to: 'tasks#index'
     
     resources :tasks
